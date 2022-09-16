fe89866fe3303a7f2138b8012f45c8aa97cfbb59
ozlemhafalir <ozlemoztep@gmail.com>
2022-09-16 10:42:55 +0000

    correct code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = documents.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
