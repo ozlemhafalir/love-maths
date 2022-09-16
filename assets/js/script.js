84110a4c0cb33da081b8e8b2ba7423594e8e7fe3
ozlemhafalir <ozlemoztep@gmail.com>
2022-09-16 08:37:14 +0000

    add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
                     let gameType = this.getAttribute("data-type");
                     alert("You clicked ${gameType}");
                 }
-            
         })
     }
 })
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
 function runGame() {
+    // creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
 }
 function checkAnswer() {
